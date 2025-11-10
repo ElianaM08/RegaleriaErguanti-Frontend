76084d6bff779a091d77015b83b55cde3611602e
Eliana Montiel <elianamontiel_10@hotmail.com>
2025-11-10 09:12:27 -0300

    Agregado de api services y purchase store y se agrego api en productStore

--- src/stores/ProductStore.js
+++ src/stores/ProductStore.js
@@ -1,36 +1,31 @@
 import { defineStore } from "pinia";
-import axios from "axios";
+import api from "../services/api";
 
-export const useProductStore = defineStore("productStore", {
+export const useProductStore = defineStore("products", {
   state: () => ({
     products: [],
-    selectedProduct: null,
   }),
 
   actions: {
     async fetchProducts() {
-      const res = await axios.get("http://localhost:3000/api/products");
-      this.products = res.data;
-    },
-
-    async fetchProductById(id) {
-      const res = await axios.get(`http://localhost:3000/api/products/${id}`);
-      this.selectedProduct = res.data;
+      const { data } = await api.get("/products");
+      this.products = data;
     },
 
     async createProduct(product) {
-      await axios.post("http://localhost:3000/api/products", product);
-      await this.fetchProducts();
+      await api.post("/products", product);
+      this.fetchProducts();
     },
 
     async updateProduct(id, product) {
-      await axios.put(`http://localhost:3000/api/products/${id}`, product);
-      await this.fetchProducts();
+      await api.put(`/products/${id}`, product);
+      this.fetchProducts();
     },
 
     async deleteProduct(id) {
-      await axios.delete(`http://localhost:3000/api/products/${id}`);
-      await this.fetchProducts();
+      await api.delete(`/products/${id}`);
+      this.fetchProducts();
     }
   }
 });
+
---
+++ src/stores/PurchaseStore.js
@@ -0,0 +1,20 @@
+import { defineStore } from "pinia";
+import api from "../services/api";
+
+export const usePurchaseStore = defineStore("purchases", {
+  state: () => ({
+    purchases: [],
+  }),
+
+  actions: {
+    async fetchPurchases() {
+      const { data } = await api.get("/purchases");
+      this.purchases = data;
+    },
+
+    async createPurchase(purchase) {
+      await api.post("/purchases", purchase);
+      this.fetchPurchases();
+    }
+  }
+});

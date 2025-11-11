be348c0553d4f53c1b9de04b2ccf9a129ba1c621
Eliana Montiel <elianamontiel_10@hotmail.com>
2025-11-11 10:26:33 -0300

    se oculto el navbar para login y register view

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <template>
   <div id="app">
-    <Navbar />
+    <Navbar  v-if="showNavbar"/>
         <router-view />
   </div>
 </template>
 <script setup>
 import Navbar from "./components/NavbarComponent.vue";
+import { computed } from "vue";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+
+const showNavbar = computed(() => {
+  return route.path !== "/login" && route.path !== "/register";
+});
 </script>
+
 <style>
 * {
   margin: 0;
